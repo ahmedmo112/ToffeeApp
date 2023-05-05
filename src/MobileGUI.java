36469f7a4af7b7ec29acaaf0e38fb28a32834506
JanaWael <Janawael652@gmail.com>
2023-05-05 21:53:23 +0300

    Added IUser, IAuthentication,Authentication, LoggedInUser, MobileGUI, UserStatus classes

---
+++ src/MobileGUI.java
@@ -0,0 +1,38 @@
+public class MobileGUI {
+    public void searchForItem() {
+        // TODO implement here
+    }
+    public void viewCategories() {
+        // TODO implement here
+    }
+    public void viewCategoryItems(int categoryID) {
+        // TODO implement here
+    }
+    public void viewItem(int itemID) {
+        // TODO implement here
+    }
+    public void addItemsToCart(int itemID) {
+        // TODO implement here
+    }
+    public void viewShoppingCart() {
+        // TODO implement here
+    }
+    public void viewOrderHistory() {
+        // TODO implement here
+    }
+    public void payOrder() {
+        // TODO implement here
+    }
+    public void register() {
+        // TODO implement here
+    }
+    public void login() {
+        // TODO implement here
+    }
+    public void logout() {
+        // TODO implement here
+    }
+    public void viewProfile() {
+        // TODO implement here
+    }
+}
