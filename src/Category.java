77aadc1740cb95759a08fd8567433db11ab7ce22
shroukk7 <shrouktarek711@gmail.com>
2023-05-05 22:02:56 +0300

    Category

---
+++ src/Category.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Category {
+    private int id;
+    private String name;
+
+    public ArrayList<Product> viewItems(Category category){
+        
+    }
+    public Product getItem(int id){
+        
+    }
+    public void setId(int id){
+
+    }
+    public int getId(){
+        return id;
+    }
+    public void setName(String name){
+
+    }
+    public String getName(){
+        return name;
+    }
+    public void AddItem(int id, String name, String brand, int price, String image, String desc, int avaliablequantity){
+
+    }
+    public void RemoveItem(Product product){
+
+    }
+    public void UpdateItem(Product product){
+
+    }
+}
---
+++ src/CategoryView.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class CategoryView implements ICategoryView{
+
+    @Override
+    public ArrayList<Category> viewCategories() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'viewCategories'");
+    }
+
+    @Override
+    public void ViewCategoryItems(Category category) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ViewCategoryItems'");
+    }
+
+    @Override
+    public void AddCategory(int id, String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'AddCategory'");
+    }
+
+}
+
